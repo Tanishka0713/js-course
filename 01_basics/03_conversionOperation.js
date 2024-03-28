34d2e09a7ecd84aed60190d00d3518b8a1493979
Tanishka0713 <111648327+Tanishka0713@users.noreply.github.com>
2024-03-28 17:36:43 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "tenishka"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "77" => 77
+// "77abc" => NaN
+// true => 1; false => 0
+
+
+
+
+let isLoggedIn = "tanishka"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+// "" => false
+// "tanishka" => true
+
+
+let someNumber = 77
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
