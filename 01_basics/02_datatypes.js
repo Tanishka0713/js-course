641ed13942e01c8265890d9e839ba1bc44d487f2
Tanishka0713 <111648327+Tanishka0713@users.noreply.github.com>
2024-03-28 17:21:20 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"    //treat all js code as newer version
+
+// alert( 3 + 3 )    //we are using noedjs not browser
+
+console.log(3+3);
+
+
+let name = "tanishka"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => jb value assign na ho
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof "tanishka");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
