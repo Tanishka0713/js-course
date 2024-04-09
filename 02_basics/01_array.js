63f99b5b289540bbe8cd45bc4c952a69fc675543
Tanishka0713 <111648327+Tanishka0713@users.noreply.github.com>
2024-04-09 18:21:15 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,24 @@
+// array
+
+const myArr = [1,2,3,4,5,6,7,8]
+console.log(myArr[2]);
+
+const arr = new Array(1,2,3,4,5)
+console.log(arr[1]);
+
+myArr.push(10)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
+myArr.unshift(11)
+console.log(myArr);
+
+myArr.shift()
+console.log(myArr);
+
+console.log(myArr.includes(7));
+console.log(myArr.indexOf(7));
+
+// Slice splice
